f9bd33d8d6728b71554587c7acedccae5813ef92
Paul Kim <paul@pnbro.com>
2019-03-19 10:07:23 +0900

    Corrected typos in defined.php

--- defined.php
+++ defined.php
@@ -5,12 +5,12 @@ if(!file_exists(dirname( __FILE__ )."/pb-config.php")){
 	die("config file not found");
 }
 
-@include(dirname( __FILE__ )."/pb-config.php");
+include(dirname( __FILE__ )."/pb-config.php");
 
 define("PB_DOCUMENT_PATH", dirname( __FILE__ )."/");
 define("PB_DOCUMENT_URL", (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER["DOCUMENT_ROOT"], "", PB_DOCUMENT_PATH));
 
-if(defined("PBDEV") && PBDEV){
+if(defined("PB_DEV") && PB_DEV){
 	define("PB_LIBRARY_PATH", PB_DOCUMENT_PATH."lib/dev/");
 	define("PB_LIBRARY_URL", PB_DOCUMENT_URL."lib/dev/");
 }else{
