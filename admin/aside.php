2d8bcea06b01d615649c195e2d8e769855ad3a00
Paul Kim <paul@pnbro.com>
2019-03-20 16:30:01 +0900

    fixed bugs

--- admin/aside.php
+++ admin/aside.php
@@ -9,13 +9,15 @@
 	
 	<ul class="pb-adminpage-list" id="pb-adminpage-list">
 		<?php foreach($adminpage_tree_ as $slug_ => $menu_data_){ ?>
-			<li class="adminpage-menu-item adminpage-menu-item-<?=$slug_?> <?=$menu_data_['active'] ? "active opened" : ""?>">
+			
 				
 				<?php if(isset($menu_data_['type']) && $menu_data_['type'] === "directory"){
 					$submenu_list_ = $menu_data_['children'];
 
 					if(count($submenu_list_) <= 0) continue;
 				?>
+
+				<li class="adminpage-menu-item adminpage-menu-item-<?=$slug_?> <?=$menu_data_['active'] ? "active opened" : ""?>">
 
 					<a href="#" data-adminpage-aside-directory="pb-adminpage-sub-list-<?=$slug_?>"><?=$menu_data_['name']?></a>
 					<ul class="pb-adminpage-sub-list" id="pb-adminpage-sub-list-<?=$slug_?>">
@@ -25,10 +27,13 @@
 							</li>
 						<?php } ?>
 					</ul>
+				</li>
 				<?php }else{ ?>
-					<a href="<?=pb_admin_url("pages/{$slug_}")?>"><?=$menu_data_['name']?></a>
+					<li>
+						<a href="<?=pb_admin_url("pages/{$slug_}")?>"><?=$menu_data_['name']?></a>
+					</li>
 				<?php } ?>
-			</li>
+			
 		<?php } ?>
 	</ul>
 
