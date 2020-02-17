93d8c03b78bd13e66e47a0edfea76614d64cd711
Paul Kim <paul@pnbro.com>
2020-02-17 17:05:03 +0900

    bugs fixed

--- defined.php
+++ defined.php
@@ -7,8 +7,6 @@ if(!file_exists(dirname( __FILE__ )."/pb-config.php")){
 	die("config file not found");
 }
 
-include(dirname( __FILE__ )."/pb-config.php");
-
 define("PB_DOCUMENT_PATH", dirname( __FILE__ )."/");
 define("PB_DOCUMENT_URL", (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . str_replace($_SERVER["DOCUMENT_ROOT"], "", PB_DOCUMENT_PATH));
 
@@ -19,5 +17,7 @@ if(defined("PB_DEV") && PB_DEV){
 	define("PB_LIBRARY_PATH", PB_DOCUMENT_PATH."lib/dist/");
 	define("PB_LIBRARY_URL", PB_DOCUMENT_URL."lib/dist/");
 }
+
+include(dirname( __FILE__ )."/pb-config.php");
 
 ?>
