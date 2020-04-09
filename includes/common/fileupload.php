d27607430069d37e69bb73c496e0b01d94827e88
Paul Kim <paul@pnbro.com>
2020-04-09 10:12:27 +0900

    Update fileupload.php
    add handler for image rotation

--- includes/common/fileupload.php
+++ includes/common/fileupload.php
@@ -57,10 +57,6 @@ function pb_fileupload_handle($files_, $options_ = array()){
 	$upload_path_ = PB_DOCUMENT_PATH."uploads".$upload_dir_;
 	$upload_url_ = PB_DOCUMENT_URL."uploads".$upload_dir_;
 
-	$max_image_width_ = defined("PB_UPLOAD_MAX_IMAGE_WIDTH") ? PB_UPLOAD_MAX_IMAGE_WIDTH : null;
-	$max_image_height_ = defined("PB_UPLOAD_MAX_IMAGE_HEIGHT") ? PB_UPLOAD_MAX_IMAGE_HEIGHT : null;
-	$image_file_types_exp_ = '/\.(gif|jpe?g|png)$/i';
-
 	$total_file_count_ = count($files_['name']);
 
 	for($file_index_= 0; $file_index_ < $total_file_count_; ++$file_index_){
@@ -109,16 +105,136 @@ function pb_fileupload_handle($files_, $options_ = array()){
 
 		$image_size_info_ = @getimagesize($origianl_file_path_);
 		$is_image_ = is_array($image_size_info_);
+		$thumbnail_ = null;
 
 		if(!file_exists($upload_path_.$yyymmdd_)){
 			mkdir($upload_path_.$yyymmdd_, 0755, true);
 		}
 
-		$move_result_ = move_uploaded_file($origianl_file_path_, $upload_path_.$yyymmdd_.$renamed_file_name_);
+		if($is_image_){
+			$max_image_width_ = defined("PB_UPLOAD_MAX_IMAGE_WIDTH") ? PB_UPLOAD_MAX_IMAGE_WIDTH : null;
+			$max_image_height_ = defined("PB_UPLOAD_MAX_IMAGE_HEIGHT") ? PB_UPLOAD_MAX_IMAGE_HEIGHT : null;
+
+			$filename = $files_['name'][$file_index_];
+			$file_path_ = $files_['tmp_name'][$file_index_];
+			$exif_data_ = @exif_read_data($file_path_);
+			$image_type_ = exif_imagetype($file_path_);
+
+			$can_rotate_ = ($image_type_ === IMAGETYPE_JPEG || $image_type_ === IMAGETYPE_PNG);
+
+			$image_resource_ = null;
+
+			switch($image_type_){
+				case IMAGETYPE_JPEG : 
+					$image_resource_ = imagecreatefromjpeg($file_path_);
+				break;
+				case IMAGETYPE_PNG : 
+					$image_resource_ = imagecreatefrompng($file_path_);
+				break;
+				case IMAGETYPE_GIF : 
+					$image_resource_ = imagecreatefromgif($file_path_);
+				break;
+			}
+
+
+			if($can_rotate_ && !empty($exif_data_['Orientation'])){
+				switch($exif_data_['Orientation']) {
+					case 3:
+					$temp_resource_ = imagerotate($image_resource_, 180, 0);
+					imagedestroy($image_resource_);
+					break;
+					case 6:
+					$temp_resource_ = imagerotate($image_resource_, -90, 0);
+					imagedestroy($image_resource_);
+					break;
+					case 8:
+					$temp_resource_ = imagerotate($image_resource_, 90, 0);
+					imagedestroy($image_resource_);
+					break;
+					default:
+					$temp_resource_ = $image_resource_;
+				}
+
+				
+				$image_resource_ = $temp_resource_;
+			}
+
+
+			$original_width_ = imagesx($image_resource_);
+			$original_height_ = imagesY($image_resource_);
+			$original_rate_ = $original_width_ / $original_height_;
+
+			//image resizing
+			if((isset($max_image_width_) && $max_image_width_ < $original_width_)
+				|| (isset($max_image_height_) && $max_image_height_ < $original_height_)){
+
+				$image_dst_width_ = $original_width_;
+				$image_dst_height_ = $original_height_;
+
+				if(isset($max_image_width_)){
+					$image_dst_width_ = $image_dst_width_ > $max_image_width_ ? $max_image_width_ : $image_dst_width_;
+					$image_dst_height_ = $image_dst_width_ / $original_rate_;
+				}
+
+				if(isset($max_image_height_)){
+					$image_dst_height_ = $image_dst_height_ > $max_image_height_ ? $max_image_height_ : $image_dst_height_;
+					$image_dst_width_ = $image_dst_height_ * ($original_rate_);
+				}
+
+				$resize_dst_instance_ = imagecreatetruecolor($image_dst_width_, $image_dst_height_);
+				imagealphablending($resize_dst_instance_, false);
+				imagesavealpha($resize_dst_instance_,true);
+				$transparent_ = imagecolorallocatealpha($resize_dst_instance_, 255, 255, 255, 127);
+				imagefilledrectangle($resize_dst_instance_, 0, 0, $image_dst_width_, $image_dst_height_, $transparent_);
+				imagecopyresampled($resize_dst_instance_, $image_resource_, 0, 0, 0, 0, $image_dst_width_, $image_dst_height_, $original_width_, $original_height_);
+
+				imagedestroy($image_resource_);
+				$image_resource_ = $resize_dst_instance_;
+			}
+
+
+			if(!file_exists($upload_path_.$yyymmdd_."thumbnail/")){
+				mkdir($upload_path_.$yyymmdd_."thumbnail/", 0755, true);
+			}
+
+			$thumbnail_file_name_ = pathinfo($renamed_file_name_, PATHINFO_FILENAME).".jpg";
+
+			//make thumbnail
+			$thumbnail_dst_width_ = $options_['thumbnail_size'];
+			$thumbnail_dst_height_ = $thumbnail_dst_width_ / $original_rate_;
+			
+			$thumbnail_dst_instance_ = imagecreatetruecolor($thumbnail_dst_width_, $thumbnail_dst_height_);
+			imagecopyresampled($thumbnail_dst_instance_, $image_resource_, 0, 0, 0, 0, $thumbnail_dst_width_, $thumbnail_dst_height_, $original_width_, $original_height_);
+
+			imagejpeg($thumbnail_dst_instance_, $upload_path_.$yyymmdd_."thumbnail/".$thumbnail_file_name_);
+			imagedestroy($thumbnail_dst_instance_);
+
+			$thumbnail_ = $yyymmdd_."thumbnail/".$thumbnail_file_name_;
+
+			switch($image_type_){
+				case IMAGETYPE_JPEG : 
+					$move_result_ = imagejpeg($image_resource_, $upload_path_.$yyymmdd_.$renamed_file_name_);
+				break;
+				case IMAGETYPE_PNG : 
+					$move_result_ = imagepng($image_resource_, $upload_path_.$yyymmdd_.$renamed_file_name_);
+				break;
+				case IMAGETYPE_GIF : 
+					$move_result_ = imagegif($image_resource_, $upload_path_.$yyymmdd_.$renamed_file_name_);
+				break;
+			}
+
+			imagedestroy($image_resource_);
+
+
+		}else{
+			$move_result_ = move_uploaded_file($origianl_file_path_, $upload_path_.$yyymmdd_.$renamed_file_name_);
+		}
+
 
 		if(!$move_result_){
 			return new PBError(UPLOAD_ERR_CANT_WRITE, "업로드실패", "파일을 쓸 수 있는 권한이 없습니다.");
 		}
+
 
 		$row_data_ = array(
 			'size' => $files_['size'][$file_index_],
@@ -131,80 +247,7 @@ function pb_fileupload_handle($files_, $options_ = array()){
 		);	
 
 		if($is_image_){
-
-			$original_src_instance_ = null;
-
-			if($file_extension_ === "gif"){
-				$original_src_instance_ = imagecreatefromgif($upload_path_.$yyymmdd_.$renamed_file_name_);
-			}else if($file_extension_ === "png"){
-				$original_src_instance_ = imagecreatefrompng($upload_path_.$yyymmdd_.$renamed_file_name_);
-			}else{
-				$original_src_instance_ = imagecreatefromjpeg($upload_path_.$yyymmdd_.$renamed_file_name_);
-			}
-
-			if(isset($original_src_instance_)){
-
-				$original_width_ = imagesx($original_src_instance_);
-				$original_height_ = imagesY($original_src_instance_);
-				$original_rate_ = $original_width_ / $original_height_;
-
-				//image resizing
-				if((isset($max_image_width_) && $max_image_width_ < $original_width_)
-					|| (isset($max_image_height_) && $max_image_height_ < $original_height_)){
-
-					$image_dst_width_ = $original_width_;
-					$image_dst_height_ = $original_height_;
-
-					if(isset($max_image_width_)){
-						$image_dst_width_ = $image_dst_width_ > $max_image_width_ ? $max_image_width_ : $image_dst_width_;
-						$image_dst_height_ = $image_dst_width_ / $original_rate_;
-					}
-
-					if(isset($max_image_height_)){
-						$image_dst_height_ = $image_dst_height_ > $max_image_height_ ? $max_image_height_ : $image_dst_height_;
-						$image_dst_width_ = $image_dst_height_ * ($original_rate_);
-					}
-
-					$resize_dst_instance_ = imagecreatetruecolor($image_dst_width_, $image_dst_height_);
-					imagealphablending($resize_dst_instance_, false);
-					imagesavealpha($resize_dst_instance_,true);
-					$transparent_ = imagecolorallocatealpha($resize_dst_instance_, 255, 255, 255, 127);
-					imagefilledrectangle($resize_dst_instance_, 0, 0, $image_dst_width_, $image_dst_height_, $transparent_);
-					imagecopyresampled($resize_dst_instance_, $original_src_instance_, 0, 0, 0, 0, $image_dst_width_, $image_dst_height_, $original_width_, $original_height_);
-
-					if($file_extension_ === "gif"){
-						$resize_dst_func_ = "imagegif";
-					}else if($file_extension_ === "png"){
-						$resize_dst_func_ = "imagepng";
-					}else{
-						$resize_dst_func_ = "imagejpeg";
-					}
-
-					call_user_func_array($resize_dst_func_, array($resize_dst_instance_, $upload_path_.$yyymmdd_.$renamed_file_name_));
-					imagedestroy($resize_dst_instance_);
-					
-				}
-
-				if(!file_exists($upload_path_.$yyymmdd_."thumbnail/")){
-					mkdir($upload_path_.$yyymmdd_."thumbnail/", 0755, true);
-				}
-
-				$thumbnail_file_name_ = pathinfo($renamed_file_name_, PATHINFO_FILENAME).".jpg";
-
-				//make thumbnail
-				$thumbnail_dst_width_ = $options_['thumbnail_size'];
-				$thumbnail_dst_height_ = $thumbnail_dst_width_ / $original_rate_;
-				
-				$thumbnail_dst_instance_ = imagecreatetruecolor($thumbnail_dst_width_, $thumbnail_dst_height_);
-				imagecopyresampled($thumbnail_dst_instance_, $original_src_instance_, 0, 0, 0, 0, $thumbnail_dst_width_, $thumbnail_dst_height_, $original_width_, $original_height_);
-
-				imagejpeg($thumbnail_dst_instance_, $upload_path_.$yyymmdd_."thumbnail/".$thumbnail_file_name_);
-
-				imagedestroy($original_src_instance_);
-				imagedestroy($thumbnail_dst_instance_);
-
-				$row_data_['thumbnail'] = $yyymmdd_."thumbnail/".$thumbnail_file_name_;
-			}
+			$row_data_['thumbnail'] = $thumbnail_;
 		}
 
 		$results_[] = $row_data_;
