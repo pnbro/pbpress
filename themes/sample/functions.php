f2dc1e8bb6779f306baf342fcbb5e2bda66f2897
Paul Kim <paul@pnbro.com>
2020-05-07 11:59:26 +0900

    Update functions.php

--- themes/sample/functions.php
+++ themes/sample/functions.php
@@ -14,6 +14,27 @@ pb_rewrite_register('other-page', array(
 	'page' => pb_current_theme_path()."other-page.php",
 ));
 
+
+pb_hook_add_filter('pb_post_types', function($results_){
+	$results_['blog'] = array(
+		'name' => '블로그',
+		'label' => array(
+			'list' => "블로그내역",
+			'add' => "블로그추가",
+			'update' => "블로그수정",
+			'delete' => "블로그삭제",
+			'button_add' => "블로그추가",
+			'button_update' => "블로그수정",
+			'before_delete' => "해당 블로그을 삭제합니다. 계속하시겠습니까?",
+			'after_delete' => "블로그가 삭제되었습니다.",
+			'no_results' => "검색된 글이 없습니다.",
+		),
+		'adminpage_sort' => 10,
+	);
+
+	return $results_;
+});
+
 include pb_current_theme_path()."includes/menu-render.php";
 include pb_current_theme_path()."includes/manage-site.php";
 include pb_current_theme_path()."includes/ajax-test.php";
