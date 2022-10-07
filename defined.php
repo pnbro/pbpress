712f731be15290f1056cbd037729b2ff1a17baa7
Paul Kim <paul@pnbro.com>
2022-10-07 11:18:00 +0900

    bugs fixed

--- defined.php
+++ defined.php
@@ -1,7 +1,7 @@
 <?php
 
-define('PB_VERSION', '3.5.0');
-define('PB_SCRIPT_VERSION', '2.2.0');
+define('PB_VERSION', '3.5.1');
+define('PB_SCRIPT_VERSION', '2.3.0');
 
 //check exists config file
 if(!file_exists(dirname( __FILE__ )."/pb-config.php")){
