801ec644dc5c401bdd1468eca68e28f62e6ee8a8
Hyeongyeon Kim <paul@pnbro.com>
2023-03-22 17:13:06 +0900

    version updated.

--- defined.php
+++ defined.php
@@ -1,7 +1,7 @@
 <?php
 
-define('PB_VERSION', '5.5.0');
-define('PB_SCRIPT_VERSION', '5.5.0');
+define('PB_VERSION', '5.6.0');
+define('PB_SCRIPT_VERSION', '5.6.0');
 
 //check exists config file
 if(!file_exists(dirname( __FILE__ )."/pb-config.php")){
