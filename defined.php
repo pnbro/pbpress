95917c8740bdf1b6607d1d2241ba1d1565d999e9
Hyeongyeon Kim <kimbobv22@gmail.com>
2023-04-04 10:27:27 +0900

    bugs fixed

--- defined.php
+++ defined.php
@@ -1,7 +1,7 @@
 <?php
 
-define('PB_VERSION', '5.7.0');
-define('PB_SCRIPT_VERSION', '5.7.0');
+define('PB_VERSION', '5.8.0');
+define('PB_SCRIPT_VERSION', '5.8.0');
 
 //check exists config file
 if(!file_exists(dirname( __FILE__ )."/pb-config.php")){
